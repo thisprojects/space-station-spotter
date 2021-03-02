2c8c6da9a3fc848a4592627bbc8d986153d0d69a
Nathan Downes <you@example.com>
2021-03-02 21:30:10 +0000

    update proxy

--- src/Utils/network-requests.js
+++ src/Utils/network-requests.js
@@ -1,11 +1,10 @@
 export const GeoLocation = () => {
   return new Promise((res, rej) => {
-
-    let timer = setTimeout(function() {
+    let timer = setTimeout(function () {
       return rej("GeoLocation Time Out");
     }, 10000);
 
-    navigator.geolocation.getCurrentPosition(pos => {
+    navigator.geolocation.getCurrentPosition((pos) => {
       clearTimeout(timer);
       const lng = pos.coords.longitude;
       const lat = pos.coords.latitude;
@@ -14,25 +13,25 @@ export const GeoLocation = () => {
   });
 };
 
-const fetchLocation = location =>
+const fetchLocation = (location) =>
   fetch(
     "https://us-central1-nathan-downes-express-api.cloudfunctions.net/api/maps",
     { method: "POST", body: `${location}` }
   )
-    .then(r => r.json())
-    .then(r => ({
+    .then((r) => r.json())
+    .then((r) => ({
       coords: r.results[0].geometry.location,
-      address: r.results[0]["formatted_address"]
+      address: r.results[0]["formatted_address"],
     }));
 
 export const callSpaceStationApi = (lat, lng) =>
   fetch(
-    `https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${lng}&alt=20&n=3`
+    `https://api.codetabs.com/v1/proxy?quest=http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${lng}&alt=20&n=3`
   )
-    .then(r => r.json())
-    .then(data => data.response || null);
+    .then((r) => r.json())
+    .then((data) => data.response || null);
 
-export const getResults = async location => {
+export const getResults = async (location) => {
   let searchLocation = location;
 
   // If no location is provided use HTML 5 geolocation API for coords
@@ -42,7 +41,7 @@ export const getResults = async location => {
 
   const {
     coords: { lat, lng },
-    address
+    address,
   } = locationData;
 
   const spaceStationResults = await callSpaceStationApi(lat, lng);
